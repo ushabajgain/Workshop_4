11868fef7c39407b4d593551b863f8ce5aa4e011
usha <ushabajgain48@gmail.com>
2025-11-18 09:28:12 +0545

    Added task 1

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,3 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using ConsoleApp4;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating first object
+        Student s1 = new Student();
+        s1.name = "Usha";
+        s1.age = 20;
+        s1.address = "Biratnagar";
+
+        // Creating second object
+        Student s2 = new Student();
+        s2.name = "Shakespeare";
+        s2.age = 22;
+        s2.address = "Pokhara";
+
+        Console.WriteLine("Student 1:");
+        Console.WriteLine("Name: " + s1.name);
+        Console.WriteLine("Age: " + s1.age);
+        Console.WriteLine("Address: " + s1.address);
+
+        Console.WriteLine("\nStudent 2:");
+        Console.WriteLine("Name: " + s2.name);
+        Console.WriteLine("Age: " + s2.age);
+        Console.WriteLine("Address: " + s2.address);
+
+        // Print static field
+        Console.WriteLine("\nSchool Name : " + Student.schoolName);
+    }
+}
--- ConsoleApp4/Student.cs
+++ ConsoleApp4/Student.cs
@@ -2,5 +2,11 @@ namespace ConsoleApp4;
 
 public class Student
 {
-    
+    // Instance fields
+    public string name;
+    public int age;
+    public string address;
+
+    // Static field
+    public static string schoolName = "Itahari International College";
 }
